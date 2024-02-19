b9c03ec570ecd512532921bc725224713fb4bfc9
ayhammaree21 <ayham.maree@gmail.com>
2024-02-20 00:17:27 +0200

    Second Task

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const IncrementCounter = ()=> {
+    setCount(count+1);
+  }
+
+  return (
+    <>
+    <button  onClick={IncrementCounter}> Ayham Maree {count}  
+    </button>
+    </>
+    
+  );
+}
+
+export default App
